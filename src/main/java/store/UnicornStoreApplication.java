2621a0659bfb5d735015ba917fc80142f861e867
IbrahimGanaMalgwi <108299271+IbrahimMalgwi@users.noreply.github.com>
2022-10-19 10:30:02 +0100

    update to codes

--- src/main/java/store/UnicornStoreApplication.java
+++ src/main/java/store/UnicornStoreApplication.java
@@ -18,70 +18,35 @@ public class UnicornStoreApplication {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        prompt();
-        int intent = getCustomerIntent();
-        processCustomerRequest(intent);
-
-    }
-
-    private static void prompt(){
-
         System.out.println("""
-                Welcome to unicorns-store. How may we help you?
-                1.  register
-                2.  login
-                3.  place order
+                Welcome to Unicorn Store. How may we be of service, today?
+                1. Register
+                2. Login
+                3. Exit
                 """);
-    }
 
-    private static int getCustomerIntent(){
-        return scanner.nextInt();
-    }
-
-    private static void processCustomerRequest(int intent){
-        switch (intent){
+        int userChoice = scanner.nextInt();
+        switch (userChoice) {
             case 1:
-                CustomerRegistrationRequest request = collectRegistrationDetails();
-                CustomerRegistrationResponse response =customerService.register(request);
-                System.out.println(response);
-                break;
-            case 2:
-                LoginRequest loginRequest = collectLoginCredentials();
-                LoginResponse loginResponse = customerService.login(loginRequest);
-                System.out.println(loginResponse);
-                break;
+                System.out.println("Enter Your Email: ");
+                String email = scanner.next();
+                System.out.println("Enter your password: ");
+                String password = scanner.next();
+                System.out.println("Enter your phone-number: ");
+                String phoneNumber = scanner.next();
+                System.out.println("Enter your address: ");
+                String address = scanner.next();
+
+                CustomerRegistrationRequest request = new CustomerRegistrationRequest();
+                request.setEmail(email);
+                request.setPassword(password);
+                request.setAddress(address);
+                request.setPhoneNumber(phoneNumber);
+
+                CustomerRegistrationResponse response = customerService.register(request);
         }
-    }
 
-    private static LoginRequest collectLoginCredentials() {
-        LoginRequest loginRequest = new LoginRequest();
-        String customerEmail = collectEmailAddress();
-        loginRequest.setEmail(customerEmail);
-        String customerPassword = collectPassword();
-        loginRequest.setPassword(customerPassword);
-        return loginRequest;
-    }
 
-    private static String collectPassword() {
-        System.out.println("Enter password: ");
-        return scanner.next();
-    }
 
-    private static String collectEmailAddress() {
-        System.out.println("Enter email: ");
-        return scanner.next();
-    }
-
-    private static CustomerRegistrationRequest collectRegistrationDetails(){
-        CustomerRegistrationRequest registrationRequest = new CustomerRegistrationRequest();
-        String customerEmail = collectEmailAddress();
-        registrationRequest.setEmail(customerEmail);
-        String customerPassword = collectPassword();
-        registrationRequest.setPassword(customerPassword);
-        System.out.println("Enter phoneNumber: ");
-        registrationRequest.setPhoneNumber(scanner.next());
-        System.out.println("Enter address: ");
-        registrationRequest.setAddress(scanner.next());
-        return registrationRequest;
     }
 }
